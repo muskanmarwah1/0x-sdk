31068d66fe23ac4c2af732efd4ee43546f2f04b5
Fannie Barskhian <barskhianfannie@gmail.com>
2022-07-21 19:12:39 -0700

    chore: add transaction module skeleton & yarn link 0x-sdk  (#14)
    * chore: add nextjs example with rainbowkit [skip actions]
    
    skip-checks:true
    
    * chore: add transaction module skeleton & yarn link 0x-sdk [skip actions]
    
    skip-checks:true

--- example/pages/_app.tsx
+++ example/pages/_app.tsx
@@ -1,11 +1,7 @@
-import '../styles/globals.css'
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
 import {
   RainbowKitProvider,
-  getDefaultWallets,
-  connectorsForWallets,
-  wallet,
 } from '@rainbow-me/rainbowkit';
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
@@ -15,11 +11,7 @@ const { chains, provider, webSocketProvider } = configureChains(
   [
     chain.mainnet,
     chain.polygon,
-    chain.optimism,
-    chain.arbitrum,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true'
-      ? [chain.goerli, chain.kovan, chain.rinkeby, chain.ropsten]
-      : []),
+    chain.ropsten
   ],
   [
     alchemyProvider({ alchemyId: '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC' }),
@@ -27,23 +19,16 @@ const { chains, provider, webSocketProvider } = configureChains(
   ]
 );
 
-const { wallets } = getDefaultWallets({
-  appName: 'RainbowKit demo',
-  chains,
-});
 
-const demoAppInfo = {
-  appName: 'Rainbowkit Demo',
-};
+import { connectorsForWallets, wallet } from '@rainbow-me/rainbowkit';
 
+
+/** TODO - Add Wallet connectors for Coinbase, etc. **/
 const connectors = connectorsForWallets([
-  ...wallets,
   {
-    groupName: 'Other',
+    groupName: 'Suggested',
     wallets: [
-      wallet.argent({ chains }),
-      wallet.trust({ chains }),
-      wallet.ledger({ chains }),
+      wallet.metaMask({ chains }),
     ],
   },
 ]);
@@ -51,13 +36,14 @@ const connectors = connectorsForWallets([
 const wagmiClient = createClient({
   autoConnect: true,
   connectors,
-  provider
+  provider,
+  webSocketProvider
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider appInfo={demoAppInfo} chains={chains}>
+      <RainbowKitProvider chains={chains}>
         <Component {...pageProps} />
       </RainbowKitProvider>
     </WagmiConfig>
--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,18 +1,81 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import type { NextPage } from 'next';
+import styles from '../styles/Home.module.css'
+import Head from 'next/head'
 
-const Home: NextPage = () => {
+const ETH = {
+  id: "ethereum",
+  symbol: "eth",
+  name: "Ethereum",
+  image: "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880",
+};
+
+const USDC = {
+  id: "usd-coin",
+  symbol: "usdc",
+  name: "USD Coin",
+  image: "https://assets.coingecko.com/coins/images/6319/large/USD_Coin_icon.png?1547042389",
+};
+
+const DAI = {
+  id: "dai",
+  symbol: "DAI",
+  name: "Dai",
+  image: "https://assets.coingecko.com/coins/images/9956/large/4943.png?1636636734",
+};
+
+const MATIC = {
+  id: "matic-network",
+  symbol: "matic",
+  name: "Polygon",
+  image: "https://assets.coingecko.com/coins/images/4713/large/matic-token-icon.png?1624446912",
+}
+
+function Home() {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'flex-end',
-        padding: 12,
-      }}
-    >
+    <div className={styles.body}>
+      <Head>
+        <title>0x SDK Example</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <ConnectButton />
+      <div className={styles.card} >
+        <h4><b>Swap Token</b></h4>
+        <div className={styles.coinmodule}>
+          <img className={styles.image} id='sellTokenIcon' src={ETH.image} alt="ethereum" ></img>
+          <label hidden htmlFor="tokenOptionSell">sell token</label>
+          <select id='tokenOptionSell'>
+            <option key="eth" value="eth">ETH</option>
+            <option key="dai" value="dai">DAI</option>
+            <option key="matic" value="matic">MATIC</option>
+          </select>
+          <label hidden htmlFor="amountToSell">sell amount</label>
+          <input id='amountToSell' type='text' placeholder='Amount' />
+        </div>
+        <div>
+          <h5>Current Balance: 99.99</h5>
+        </div>
+      </div>
+      <div>&darr;</div>
+      <div className={styles.card}>
+        <h4><b>Buy Token</b></h4>
+        <div className={styles.coinmodule}>
+          <img className={styles.image} src={MATIC.image} alt="matic" ></img>
+          <label hidden htmlFor="tokenOptionBuy">buy token</label>
+          <select
+            id='tokenOptionBuy'>
+            <option key="eth" value="eth">ETH</option>
+            <option key="dai" value="dai">DAI</option>
+            <option key="matic" value="matic">MATIC</option>
+          </select>
+          <button type="button">Quote</button>
+        </div>
+        <div>
+          <h5>Quoted Amount: 99.99 MATIC</h5>
+        </div>
+      </div>
     </div>
-  );
+  )
 };
 
 export default Home;
+
