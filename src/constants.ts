2411ad6a6f7b4e375adb4ed2d5e854a6792f817c
Jade <jadetxyz@gmail.com>
2022-08-18 18:22:20 -0400

    fix: update api endpoints (#32)

--- src/constants.ts
+++ src/constants.ts
@@ -8,8 +8,8 @@ export const ROOT_STAGING_URL = 'https://staging.api.0x.org';
 export const ROOT_URLS_BY_CHAIN_ID: { [key: string]: string } = {
   '1': 'https://api.0x.org',
   '3': 'https://ropsten.api.0x.org',
-  '137': 'https://polygon-api.matcha.0x.org',
-  '56': 'https://bsc-api.matcha.0x.org',
+  '137': 'https://polygon-api.0x.org',
+  '56': 'https://bsc-api.0x.org',
   '10': 'https://optimism.api.0x.org',
   '250': 'https://fantom.api.0x.org',
   '42220': 'https://celo.api.0x.org',
